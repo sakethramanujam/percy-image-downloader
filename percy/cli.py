c7b394de550887afdf71df020fd5ba13d286eaa4
Saketha Ramanjam <ramanujamsamavedam@hotmail.com>
2021-02-22 22:15:44 +0000

    add cli.py

---
+++ percy/cli.py
@@ -0,0 +1,25 @@
+import click
+from click import formatting
+from click_default_group import DefaultGroup
+from .version import __version__
+from .percy import create_percy
+
+
+@click.group()
+@click.version_option(version=__version__)
+def cli():
+    """
+    Image Downloader for Percy the Mars Rover
+    """
+
+
+@cli.command()
+@click.option('-r', '--resolution', help="Parameter for left column", nargs=1, required=True)
+@click.option('-p', '--path', default="./perseverance", help="Parameter for left column", nargs=1,
+              required=True)
+@click.option('-n', '--number', default=0, help="Page Number from which images are to be downloaded, defaults to 0", nargs=1)
+def download(resolution, path, number):
+    percy = create_percy(resolution=resolution,
+                         basepath=path,
+                         page_num=number)
+    percy.download()
